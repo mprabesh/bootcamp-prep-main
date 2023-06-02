9a4ecb60e36f73d607ff9ed40ca0291e9e4cdf8f
mprabesh <theprabesh723@gmail.com>
2023-06-02 13:43:22 +0545

    04-control-flow solved

--- pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
+++ pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
@@ -1,7 +1,12 @@
-let name = 'David';
+// let name= 'David';
 // let name = 'Nimit';
-// let name = 'Someone else';
+let name = 'Someone else';
 
 let found;
 
-// YOUR CODE BELOW
+if(name==='David'||name==='Nimit'){
+    found=true
+}else{
+    found=false
+}
+
