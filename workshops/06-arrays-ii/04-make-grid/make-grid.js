1003293193bc48aa2dbc1672fdd5c3afde966cd1
mprabesh <theprabesh723@gmail.com>
2023-06-02 22:51:09 +0545

    05-remove-columns-done

--- workshops/06-arrays-ii/04-make-grid/make-grid.js
+++ workshops/06-arrays-ii/04-make-grid/make-grid.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+const makeGrid=(numC,numR)=>{
+    let arr=[]
+    for(let i=0;i<numR;i++){
+        arr[i]=[]
+        for(let k=0;k<numC;k++){
+            arr[i][k]=k+1
+
+        }
+    }
+    console.log(arr)
+    return arr
+}
+
+makeGrid(6,4)
--- workshops/06-arrays-ii/05-remove-columns/remove-columns.js
+++ workshops/06-arrays-ii/05-remove-columns/remove-columns.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+const removeColumns=(arr2,numC)=>{
+for(let i=0;i<arr2.length;i++){
+    for(k=0;k<numC;k++){
+        arr2[i].pop()
+    }
+}
+return (arr2)
+
+}
+
