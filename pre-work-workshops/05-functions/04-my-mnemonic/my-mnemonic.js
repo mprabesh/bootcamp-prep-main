c2c8062ff5558666f887aa1272733333551272c8
mprabesh <theprabesh723@gmail.com>
2023-06-05 14:19:12 +0545

    my-mneumonics task optimized

--- pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
+++ pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
@@ -1,22 +1,34 @@
 //YOUR CODE BELOW
-const myMnemonic=(firstWord,secondWord,thirdWord,fourthWord)=>{
-    let finalString='';
-    if(firstWord !== undefined){
-        finalString += firstWord[0]
-    }
-    console.log(finalString)
-    if(secondWord !== undefined){
-        finalString += secondWord[0]
-    }
-    if(thirdWord !== undefined){
-        finalString += thirdWord[0]
-    }
-    if(fourthWord !== undefined){
-        finalString += fourthWord[0]
-    }
+// const myMnemonic = (firstWord, secondWord, thirdWord, fourthWord) => {
+//   let finalString = "";
+//   console.log(arguments[0]);
+//   for (let i = 0; i < argument.length; i++) {
+//     console.log(argument[i]);
+//   }
+//   if (firstWord !== undefined) {
+//     finalString += firstWord[0];
+//   }
+//   // console.log(finalString)
+//   if (secondWord !== undefined) {
+//     finalString += secondWord[0];
+//   }
+//   if (thirdWord !== undefined) {
+//     finalString += thirdWord[0];
+//   }
+//   if (fourthWord !== undefined) {
+//     finalString += fourthWord[0];
+//   }
 
-    return finalString
+//   return finalString;
+// };
+
+//using arguments only ...
+function myMnemonic(firstWord, secondWord, thirdWord, fourthWord) {
+  let final_string = "";
+  for (let i = 0; i < arguments.length; i++) {
+    final_string += arguments[i][0];
+  }
+  console.log(final_string);
+  return final_string;
 }
-
-myMnemonic('apple','orange','mango')
-
+myMnemonic("Must");
