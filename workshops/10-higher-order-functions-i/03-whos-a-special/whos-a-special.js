62ddad1c4af2e4a1b98fc21c3aa35e24351fffa1
mprabesh <theprabesh723@gmail.com>
2023-06-16 18:50:38 +0545

    whos-a-special.js optimized

--- workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
+++ workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
@@ -1,40 +1,14 @@
 // YOUR CODE BELOW
-let new_val = []
 function whosASpecial(arrPets) {
+    let animalArray = []
     let new_string = ''
-    while (new_val.length) {
-        new_val.pop()
-    }
-    arrPets.forEach(mySpecial)
-
-    for (let k = 0; k < new_val.length; k++) {
-        if (k !== new_val.length - 1) {
-            new_string = new_string + new_val[k] + ' '
-        } else {
-            new_string = new_string + new_val[k]
-        }
-    }
-
+    arrPets.forEach(function (para) {
+        let animal = para['name']
+        let species = para['species']
+        let specialString = `${animal} the ${species} is very special!`
+        animalArray.push(specialString)
+    })
+    new_string = animalArray.join(' ')
     console.log(new_string)
     return new_string
 }
-
-function mySpecial(objP) {
-    let specialString = ''
-    let name = objP['name']
-    let species = objP['species']
-    specialString = `${name} the ${species} is very special!`
-    new_val.push(specialString)
-}
-
-let specialPets = [
-    {
-        name: 'Mr. Binns',
-        species: 'cat',
-    },
-    {
-        name: 'Pusheen',
-        species: 'cat',
-    },
-]
-whosASpecial(specialPets)
