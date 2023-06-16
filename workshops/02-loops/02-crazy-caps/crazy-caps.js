994ad558f25daf12d742ff6ced93db007cab627e
mprabesh <theprabesh723@gmail.com>
2023-06-16 21:14:35 +0545

    crazy-caps completed

--- workshops/02-loops/02-crazy-caps/crazy-caps.js
+++ workshops/02-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+function crazyCaps(stringVal) {
+    let newString = ''
+    for (let k = 0; k < stringVal.length; k++) {
+        let changedChar = ''
+        if (k % 2) {
+            changedChar = stringVal[k].toUpperCase()
+            newString += changedChar
+        } else {
+            changedChar = stringVal[k].toLowerCase()
+            newString += changedChar
+        }
+    }
+    console.log(newString)
+    return newString
+}
+
+crazyCaps('fullstack is amazing')
