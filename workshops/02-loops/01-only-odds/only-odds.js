1f31d3b27edd27cf438a7f88e30025a9a64d593f
mprabesh <theprabesh723@gmail.com>
2023-06-16 18:57:55 +0545

    01-only-odds completed

--- workshops/02-loops/01-only-odds/only-odds.js
+++ workshops/02-loops/01-only-odds/only-odds.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function onlyOdds(num) {
+    let oddSum = 0
+    if (num < 1) {
+        return 0
+    }
+    for (let k = 1; k <= num; k++) {
+        if (k % 2) {
+            oddSum += k
+        }
+    }
+    return oddSum
+}
+
+onlyOdds(9)
